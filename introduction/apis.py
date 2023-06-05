7f3f4648e47e411d4e096dd6b4f09fbe9f21db5c
Ben Lloyd Pearson <ben.pearson@linearb.io>
2023-06-05 17:36:45 -0400

    Unsafe PR

--- introduction/apis.py
+++ introduction/apis.py
@@ -4,6 +4,7 @@ from introduction.playground.ssrf import main
 from introduction.playground.A9.main import Log
 from introduction.playground.A6.utility import check_vuln
 from django.contrib.auth import login,authenticate
+from .utility import *
 from django.views.decorators.csrf import csrf_exempt
 import time
 from .views import authentication_decorator
@@ -17,6 +18,29 @@ import requests
 def ssrf_code_checker(request):
     if request.user.is_authenticated:
         if request.method == 'POST':
+            python_code = request.POST['python_code']
+            html_code = request.POST['html_code']
+            if not (ssrf_code_converter(python_code)):
+                return JsonResponse({"status": "error", "message": "Invalid code"})
+            test_bench1 = ssrf_html_input_extractor(html_code)
+
+            if (len(test_bench1) >4):
+                return JsonResponse({'message':'too many inputs in Html\n Try again'},status = 400)
+            test_bench2 = ['secret.txt']
+            correct_output1 = [{"blog": "blog1-passed"}, {"blog": "blog2-passed"}, {"blog": "blog3-passed"}, {"blog": "blog4-passed"}]
+            outputs = []
+            for inputs in test_bench1:
+                outputs.append(main.ssrf_lab(inputs))
+            if outputs == correct_output1:
+                outputs = []
+            else:
+                return JsonResponse({'message':'Testbench failed, Code is not working\n Try again'},status = 200)
+
+            correct_output2 = [{"blog": "No blog found"}]
+            for inputs in test_bench2:
+                outputs.append(main.ssrf_lab(inputs))
+            if outputs == correct_output2:
+                return JsonResponse({'message':'Congratulation, you have written a secure code.', 'passed':1}, status = 200)
             
             return JsonResponse({'message':'Test bench passed but the code is not secure'}, status = 200,safe = False)
         else:
